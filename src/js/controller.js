05735df4e208e9635e5ecb582b4b64f0bfe75e0e
fahrilmi25 <fahrilmi25@gmail.com>
2026-05-07 08:36:36 +0800

    Feat: Implement pagination functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -5,6 +5,7 @@ import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
 import resultsView from './views/resultsView.js';
+import paginationView from './views/paginationView.js';
 
 // Polyfills to support older browsers (e.g., IE11)
 // polyfiling everyting
@@ -46,14 +47,25 @@ const controlSearchResults = async function () {
 
     // 3) render results
     resultsView.render(model.getSearchResultsPage());
+
+    // 4) render initial pagination buttons
+    paginationView.render(model.state.search);
   } catch (error) {
     resultsView.renderError();
   }
-};  
+};
 
+const controlPagination = function (goToPage) {
+  // 1) render NEW results
+  resultsView.render(model.getSearchResultsPage(goToPage));
+
+  // 2) render NEW pagination buttons
+  paginationView.render(model.state.search);
+};
 // Subscriber
 const init = function () {
   recipeView.addHandlerRender(controlRecipe);
   searchView.addHandleSearch(controlSearchResults);
+  paginationView._addHandlerClick(controlPagination);
 };
 init();
---
+++ src/js/views/paginationView.js
@@ -0,0 +1,82 @@
+import View from './view';
+import icons from 'url:../../img/icons.svg'; // parcel 2
+
+class PaginationView extends View {
+  _parentElement = document.querySelector('.pagination');
+
+  _addHandlerClick(handler) {
+    this._parentElement.addEventListener('click', function (e) {
+      const btn = e.target.closest('.btn--inline');
+      if (!btn) return;
+      const goToPage = +btn.dataset.goto;
+
+      handler(goToPage);
+    });
+  }
+
+  _generateMarkup() {
+    const currentPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage,
+    );
+
+    // Page 1: there are other page
+    if (currentPage === 1 && numPages > 1) {
+      return this.__generateMarkupPagination(currentPage, 'next');
+    }
+
+    // Last page
+    if (currentPage === numPages && numPages > 1) {
+      // Page 1: there are NO other page
+      return this.__generateMarkupPagination(currentPage, 'previous');
+    }
+
+    // Other page
+    if (currentPage < numPages) {
+      return this.__generateMarkupPagination(currentPage, 'previousNext');
+    }
+
+    // Page 1: there are NO other page
+    return ``;
+  }
+
+  __generateMarkupPagination(currPage, previosOrNext) {
+    if (previosOrNext === 'next') {
+      return `
+      <button data-goto="${currPage + 1}" class="btn--inline pagination__btn--next">
+        <span>Page ${currPage + 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>`;
+    }
+
+    if (previosOrNext === 'previous') {
+      return `
+      <button data-goto="${currPage - 1}" class="btn--inline pagination__btn--prev">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${currPage - 1}</span>
+      </button>`;
+    }
+
+    if (previosOrNext === 'previousNext') {
+      return `
+      <button data-goto="${currPage - 1}" class="btn--inline pagination__btn--prev">
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-left"></use>
+        </svg>
+        <span>Page ${currPage - 1}</span>
+      </button>
+      <button data-goto="${currPage + 1}" class="btn--inline pagination__btn--next">
+        <span>Page ${currPage + 1}</span>
+        <svg class="search__icon">
+          <use href="${icons}#icon-arrow-right"></use>
+        </svg>
+      </button>`;
+    }
+  }
+}
+
+export default new PaginationView();
